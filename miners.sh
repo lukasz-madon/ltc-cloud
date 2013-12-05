59efaca2ab05310a0102b95b8980e5f5e87af1e7
Lukasz Madon <lukasz.madon@gmail.com>
2013-12-05 10:44:02 +0000

    no logs

--- miners.sh
+++ miners.sh
@@ -3,14 +3,14 @@
 sudo yum -y groupinstall "Development Tools"
 sudo yum -y install git libcurl-devel python-devel screen rsync yasm numpy openssl-devel
 wget http://developer.download.nvidia.com/compute/cuda/5_5/rel/installers/cuda_5.5.22_linux_64.run
-sudo sh cuda_5.5.22_linux_64.run  silent -toolkit -driver creates=/usr/local/cuda-5.5
+sudo sh cuda_5.5.22_linux_64.run -silent -toolkit -driver creates=/usr/local/cuda-5.5
 # CudaMiner
 git clone https://github.com/cbuchner1/CudaMiner
 git reset -–hard 88c6da6d5c2b798d1de7031e8dbcc2678f635e4b
 cd CudaMiner
 ./configure
 PATH=/usr/local/cuda-5.5/bin:$PATH make
-nohup sh -c "/usr/bin/env LD_LIBRARY_PATH=/usr/local/cuda-5.5/lib64 ./cudaminer -o stratum+tcp://us3.wemineltc.com:3333 -u lukasgt.gpu3 -p gpu3 -C 1 &"
+nohup sh -c "/usr/bin/env LD_LIBRARY_PATH=/usr/local/cuda-5.5/lib64 ./cudaminer -o stratum+tcp://us3.wemineltc.com:3333 -u lukasgt.gpu2 -p gpu2 -C 1 &" >/dev/null 2>&1
 # proxy
 cd ~
 git clone https://github.com/bandroidx/stratum-mining-proxy
@@ -21,7 +21,7 @@ sudo python setup.py install
 cd ..
 sudo python setup.py develop
 chmod +x mining_proxy.py
-nohup sh -c "./mining_proxy.py -o stratum2.wemineltc.com -p 3333 &" 
+nohup sh -c "./mining_proxy.py -o stratum2.wemineltc.com -p 3333 &" >/dev/null 2>&1
 # CPU miner
 cd ~
 git clone https://github.com/pooler/cpuminer
@@ -29,4 +29,4 @@ cd cpuminer/
 ./autogen.sh
 ./configure CFLAGS="-O3"
 make
-nohup sh -c "./minerd -o http://127.0.0.1:8332 -u lukasgt.cpu3 -p cpu3 -t 6 &"
+nohup sh -c "./minerd -o http://127.0.0.1:8332 -u lukasgt.cpu2 -p cpu2 -t 6 &" >/dev/null 2>&1
