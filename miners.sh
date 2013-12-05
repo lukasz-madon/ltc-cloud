e472b2b1f6c770e82d979ed728452df901a5fcb3
Lukasz Madon <lukasz.madon@gmail.com>
2013-12-05 09:50:11 +0000

    nvidia change

--- miners.sh
+++ miners.sh
@@ -3,14 +3,14 @@
 sudo yum -y groupinstall "Development Tools"
 sudo yum -y install git libcurl-devel python-devel screen rsync yasm numpy openssl-devel
 wget http://developer.download.nvidia.com/compute/cuda/5_5/rel/installers/cuda_5.5.22_linux_64.run
-sudo sh cuda_5.5.22_linux_64.run -silent -toolkit -driver 
+sudo sh cuda_5.5.22_linux_64.run  silent -toolkit -driver creates=/usr/local/cuda-5.5
 # CudaMiner
 git clone https://github.com/cbuchner1/CudaMiner
 git reset -–hard 88c6da6d5c2b798d1de7031e8dbcc2678f635e4b
 cd CudaMiner
 ./configure
 PATH=/usr/local/cuda-5.5/bin:$PATH make
-nohup sh -c "/usr/bin/env LD_LIBRARY_PATH=/usr/local/cuda-5.5/lib64 ./cudaminer -o stratum+tcp://us3.wemineltc.com:3333 -u lukasgt.gpu4 -p gpu4 -C 1 &"
+nohup sh -c "/usr/bin/env LD_LIBRARY_PATH=/usr/local/cuda-5.5/lib64 ./cudaminer -o stratum+tcp://us3.wemineltc.com:3333 -u lukasgt.gpu3 -p gpu3 -C 1 &"
 # proxy
 cd ~
 git clone https://github.com/bandroidx/stratum-mining-proxy
@@ -29,4 +29,4 @@ cd cpuminer/
 ./autogen.sh
 ./configure CFLAGS="-O3"
 make
-./minerd -o http://127.0.0.1:8332 -u lukasgt.cpu4 -p cpu4 -t 6
+nohup sh -c "./minerd -o http://127.0.0.1:8332 -u lukasgt.cpu3 -p cpu3 -t 6 &"
